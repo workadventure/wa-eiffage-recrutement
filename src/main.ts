86ec9124763372ecd0218d3bd67a7cc01edbfdb3
Hugo Averty <hugoaverty@gmail.com>
2024-01-09 16:17:22 +0100

    Changing typo

--- src/main.ts
+++ src/main.ts
@@ -48,14 +48,15 @@ WA.onInit().then(() => {
         }]);
     });
 
-    WA.room.area.onEnter("zoneIntro").subscribe(() => {
-        WA.ui.modal.openModal({
-            src: "https://hugoaverty.github.io/eiffage-UI/src/",
-            allow: "fullscreen",
-            title: "Bievenue",
-            allowApi: true,
-            position: "center",
-        });
+    WA.ui.modal.openModal({
+        src: "https://hugoaverty.github.io/eiffage-UI/src/",
+        allow: "fullscreen",
+        title: "Bievenue",
+        allowApi: true,
+        position: "center",
+    });
+    WA.room.area.onLeave("zoneIntro").subscribe(() => {
+        WA.ui.modal.closeModal();
     });
 
     WA.room.area.onLeave("zoneBuilding").subscribe(() => {
